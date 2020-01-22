28d39c8d1f0e7ab2c2c44b00062dccb25f079e50
MinexAutomation <minex.automation@gmail.com>
2020-01-22 15:13:20 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Chalandri.DropboxRivetTestingData.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Chalandri.DropboxRivetTestingData.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/R5T.Chalandri.DropboxRivetTestingData/Code/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace R5T.Chalandri.DropboxRivetTestingData
+{
+    public class Class1
+    {
+    }
+}
