0a1e340e24eed77ce7019b4bdaee3da9bb658805
MinexAutomation <minex.automation@gmail.com>
2020-01-22 15:25:46 -0800

    Added IServiceCollection extensions.

--- source/R5T.Chalandri.DropboxRivetTestingData/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Chalandri.DropboxRivetTestingData
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Chalandri.DropboxRivetTestingData/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Chalandri.Glenrothes;
+using R5T.Glenrothes.DropboxRivetData;
+using R5T.Ilioupoli.Standard;
+using R5T.Lombardy.Standard;
+
+
+namespace R5T.Chalandri.DropboxRivetTestingData
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTestingDataDirectoryContentPathsProvider(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<ITestingDataDirectoryContentPathsProvider, TestingDataDirectoryContentPathsProvider>()
+                .AddTestingDataDirectoryPathProvider()
+                .AddTestingDataDirectoryContentConventions()
+                .AddStringlyTypedPathOperator()
+                ;
+
+            return services;
+        }
+    }
+}
