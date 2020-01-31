c8d15ebd52b592e6ae41ada375921843bf4cbb59
MinexAutomation <minex.automation@gmail.com>
2020-01-30 16:35:08 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Chalandri.DropboxRivetTestingData/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Chalandri.DropboxRivetTestingData/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -3,6 +3,7 @@
 using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Chalandri.Glenrothes;
+using R5T.Dacia;
 using R5T.Glenrothes.DropboxRivetData;
 using R5T.Ilioupoli.Standard;
 using R5T.Lombardy.Standard;
@@ -17,14 +18,22 @@ namespace R5T.Chalandri.DropboxRivetTestingData
         /// </summary>
         public static IServiceCollection AddTestingDataDirectoryContentPathsProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<ITestingDataDirectoryContentPathsProvider, TestingDataDirectoryContentPathsProvider>()
-                .AddTestingDataDirectoryPathProvider()
-                .AddTestingDataDirectoryContentConventions()
-                .AddStringlyTypedPathOperator()
+            services.AddGlenrothesTestingDataDirectoryContentPathsProvider(
+                services.AddTestingDataDirectoryPathProviderAction(),
+                services.AddTestingDataDirectoryContentConventionsAction(),
+                services.AddStringlyTypedPathOperatorAction())
                 ;
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds an <see cref="ITestingDataDirectoryContentPathsProvider"/> service.
+        /// </summary>
+        public static ServiceAction<ITestingDataDirectoryContentPathsProvider> AddTestingDataDirectoryContentPathsProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<ITestingDataDirectoryContentPathsProvider>(() => services.AddTestingDataDirectoryContentPathsProvider());
+            return serviceAction;
         }
     }
 }
